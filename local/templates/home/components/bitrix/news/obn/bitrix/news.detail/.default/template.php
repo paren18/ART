b2d501622146f4dd741cc82cd82e59b596b2780d
paren18 <igor.zelenov.1998@mail.ru>
2024-04-13 01:22:32 +0300

    task 4.5

---
+++ local/templates/home/components/bitrix/news/obn/bitrix/news.detail/.default/template.php
@@ -0,0 +1,161 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<?php if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arResult["DETAIL_PICTURE"])): ?>
+<div class="site-blocks-cover overlay" style="background-image: url('<?=$arResult["DETAIL_PICTURE"]["SRC"]?>');" data-aos="fade" data-stellar-background-ratio="0.5">
+    <?php endif; ?>
+
+    <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-10">
+                <span class="d-inline-block text-white px-3 mb-3 property-offer-type rounded">Property Details of</span>
+                <h1 class="mb-2"><?=$arResult["NAME"]?></h1>
+                <p class="mb-5"><strong class="h2 text-success font-weight-bold">₽<?=$arResult["PROPERTIES"]["PRICE"]["VALUE"]?></strong></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="site-section site-section-sm">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8" style="margin-top: -150px;">
+                <div class="mb-5">
+                    <div class="slide-one-item home-slider owl-carousel">
+                        <?php foreach ($arResult["DISPLAY_PROPERTIES"]["GALERPICS"]["VALUE"] as $imageID): ?>
+                            <div><img src="<?= CFile::GetPath($imageID) ?>" alt="Image" class="img-fluid"></div>
+                        <?php endforeach; ?>
+                    </div>
+
+
+                </div>
+                <div class="bg-white">
+                    <div class="row mb-5">
+                        <div class="col-md-6">
+                            <strong class="text-success h1 mb-3">₽<?=$arResult["PROPERTIES"]["PRICE"]["VALUE"]?></strong>
+                        </div>
+                        <div class="col-md-6">
+                            <ul class="property-specs-wrap mb-3 mb-lg-0  float-lg-right">
+                                <li>
+                                    <span class="property-specs">Дата изменения</span>
+                                    <span class="property-specs-number"><?= $arResult["TIMESTAMP_X"]?></span>
+
+                                </li>
+                                <li>
+                                    <span class="property-specs">Этажи</span>
+                                    <span class="property-specs-number"><?=$arResult["DISPLAY_PROPERTIES"]["NUMFLOORS"]["VALUE"]?></span>
+
+                                </li>
+                                <li>
+                                    <span class="property-specs">Общая площадь</span>
+                                    <span class="property-specs-number"><?=$arResult["PROPERTIES"]["TOTALAREA"]["VALUE"]?></span>
+
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row mb-5">
+                        <div class="col-md-6 col-lg-4 text-left border-bottom border-top py-3">
+                            <span class="d-inline-block text-black mb-0 caption-text">Количество санузлов</span>
+                            <strong class="d-block"><?=$arResult["DISPLAY_PROPERTIES"]["NUMOFBATH"]["VALUE"]?></strong>
+                        </div>
+                        <div class="col-md-6 col-lg-4 text-left border-bottom border-top py-3">
+                            <span class="d-inline-block text-black mb-0 caption-text">Наличие гаража</span>
+                            <strong class="d-block"><?=$arResult["DISPLAY_PROPERTIES"]["AVAOFGARAGE"]["VALUE"]?></strong>
+                        </div>
+                    </div>
+                    <h2 class="h4 text-black">Детальное описание</h2>
+                    <p><?=$arResult["DETAIL_TEXT"]?></p>
+
+                    <div class="row mt-5">
+                        <div class="col-12">
+                            <h2 class="h4 text-black mb-3">Галерея изображений</h2>
+                        </div>
+                        <div class="row mt-5">
+                            <?php foreach ($arResult["DISPLAY_PROPERTIES"]["GALERPICS"]["VALUE"] as $imageID): ?>
+                                <?php
+                                $imageSrc = CFile::GetPath($imageID);
+                                ?>
+                                <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                                    <a href="<?= $imageSrc ?>" class="image-popup gal-item">
+                                        <img src="<?= $imageSrc ?>" alt="Image" class="img-fluid">
+                                    </a>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-12">
+                            <h4 class="text-black">Дополнительные материалы</h4>
+                           <div class="h4 text-black"><?php
+                               foreach ($arResult['PROPERTIES']['DOPMAT']['VALUE'] as $dop) {
+                                   echo "$dop<br>";
+                               }
+                               ?></div>
+                        </div>
+                    </div>
+
+
+
+                    <!-- Блок "Ссылки на внешние ресурсы" -->
+                    <div class="row mt-5">
+                        <div class="col-md-12">
+                            <h2 class="h4 text-black">Ссылки на внешние ресурсы</h2>
+                            <div class="h4 text-black"><?php
+                                foreach ($arResult['PROPERTIES']['LINKS']['VALUE'] as $link) {
+                                    echo "<a href='$link'>$link</a><br>";
+                                }
+                                ?></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div class="col-lg-4 pl-md-5">
+
+                <div class="bg-white widget border rounded">
+
+                    <h3 class="h4 text-black widget-title mb-3">Contact Agent</h3>
+                    <form action="" class="form-contact-agent">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" id="name" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" id="email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Phone</label>
+                            <input type="text" id="phone" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" id="phone" class="btn btn-primary" value="Send Message">
+                        </div>
+                    </form>
+                </div>
+
+                <div class="bg-white widget border rounded">
+                    <h3 class="h4 text-black widget-title mb-3">Paragraph</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit qui explicabo, libero nam, saepe eligendi. Molestias maiores illum error rerum. Exercitationem ullam saepe, minus, reiciendis ducimus quis. Illo, quisquam, veritatis.</p>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ local/templates/home/components/bitrix/news/obn/bitrix/news.list/.default/template.php
@@ -0,0 +1,177 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="pt-5">
+    <div class="container">
+        <form class="row">
+
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Lot Area</option>
+                        <option value="1000">1000</option>
+                        <option value="800">800</option>
+                        <option value="600">600</option>
+                        <option value="400">400</option>
+                        <option value="200">200</option>
+                        <option value="100">100</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Property Status</option>
+                        <option value="For Sale">For Sale</option>
+                        <option value="For Rent">For Rent</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Location</option>
+                        <option value="United States">United States</option>
+                        <option value="United Kingdom">United Kingdom</option>
+                        <option value="Canada">Canada</option>
+                        <option value="Belgium">Belgium</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Lot Area</option>
+                        <option value="1000">1000</option>
+                        <option value="800">800</option>
+                        <option value="600">600</option>
+                        <option value="400">400</option>
+                        <option value="200">200</option>
+                        <option value="100">100</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Bedrooms</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5+">5+</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="select-wrap">
+                    <span class="icon icon-arrow_drop_down"></span>
+                    <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                        <option value="">Bathrooms</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5+">5+</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div class="mb-4">
+                    <div id="slider-range" class="border-primary ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"><div class="ui-slider-range ui-corner-all ui-widget-header" style="left: 19.1919%; width: 55.5556%;"></div><span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default" style="left: 19.1919%;"></span><span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default" style="left: 74.7475%;"></span></div>
+                    <input type="text" name="text" id="amount" class="form-control border-0 pl-0 bg-white" disabled="">
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+                <input type="submit" class="btn btn-primary btn-block form-control-same-height rounded-0" value="Search">
+            </div>
+
+        </form>
+
+
+    </div>
+</div>
+<div class="site-section site-section-sm bg-light">
+<div class="container">
+    <div class="row mb-5">
+        <div class="col-12">
+            <div class="site-section-title">
+                <h2>New Properties for You</h2>
+            </div>
+        </div>
+    </div>
+    <div class="row mb-5">
+        <?foreach($arResult["ITEMS"] as $arItem):?>
+            <div class="col-md-6 col-lg-4 mb-4">
+                <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="prop-entry d-block">
+                    <figure>
+                        <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>" class="img-fluid">
+                    </figure>
+                    <div class="prop-text">
+                        <div class="inner">
+                            <span class="price rounded">₽<?=$arItem["PROPERTIES"]["PRICE"]["VALUE"]?></span>
+                            <h3 class="title"><?=$arItem["NAME"]?></h3>
+                            <p class="location"><?=$arItem["DISPLAY_PROPERTIES"]["LOCATION"]["VALUE"]?></p>
+                        </div>
+                        <div class="prop-more-info">
+                            <div class="inner d-flex">
+                                <div class="col">
+                                    <span>Площадь:</span>
+                                    <strong><?=$arItem["DISPLAY_PROPERTIES"]["TOTALAREA"]["VALUE"]?></strong>
+                                </div>
+                                <div class="col">
+                                    <span>Этажи:</span>
+                                    <strong><?=$arItem["DISPLAY_PROPERTIES"]["NUMFLOORS"]["VALUE"]?></strong>
+                                </div>
+                                <div class="col">
+                                    <span>Ванна:</span>
+                                    <strong><?=$arItem["DISPLAY_PROPERTIES"]["NUMOFBATH"]["VALUE"]?></strong>
+                                </div>
+                                <div class="col">
+                                    <span>Гараж:</span>
+                                    <strong><?=$arItem["DISPLAY_PROPERTIES"]["AVAOFGARAGE"]["VALUE"]?></strong>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        <?endforeach;?>
+    </div>
+    <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="site-pagination">
+                    <?=$arResult["NAV_STRING"]?>
+                </div>
+            </div>
+        </div>
+    <?endif;?>
+</div>
+    <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="site-pagination">
+                    <?=$arResult["NAV_STRING"]?>
+                </div>
+            </div>
+        </div>
+    <?endif;?>
+</div>
+
