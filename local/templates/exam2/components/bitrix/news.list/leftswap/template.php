b26c4e8202541d03a59ca20b9604c4c89dd7e9c7
paren18 <igor.zelenov.1998@mail.ru>
2024-05-02 22:47:20 +0300

    task 4.2

--- local/templates/exam2/components/bitrix/news.list/leftswap/template.php
+++ local/templates/exam2/components/bitrix/news.list/leftswap/template.php
@@ -13,32 +13,33 @@
 $this->setFrameMode(true);
 ?>
 
-<div class="rew-footer-carousel">
-    <?foreach($arResult["ITEMS"] as $arItem):?>
-    <div class="item">
-        <div class="side-block side-opin">
-            <div class="inner-block">
-                <div class="title">
-                    <div class="photo-block">
-                        <? if(!empty($arItem["PREVIEW_PICTURE"]["SRC"])):?>
-                        <img width="39" height="39" src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="">
-                        <? else:?>
-                            <img width="39" height="39" src="<?= SITE_TEMPLATE_PATH ?>/img/rew/no_photo.jpg" alt="img">
-                        <?endif;?>
+<div class="item-wrap">
+    <div class="rew-footer-carousel">
+        <?foreach($arResult["ITEMS"] as $arItem):?>
+            <div class="item">
+                <div class="side-block side-opin">
+                    <div class="inner-block">
+                        <div class="title">
+                            <div class="photo-block">
+                                <?php if($arItem["PREVIEW_PICTURE"]["SRC"]):?>
+                                    <?
+                                    $resizedImage = CFile::ResizeImageGet($arItem["PREVIEW_PICTURE"]["ID"], array("width" => 49, "height" => 49), BX_RESIZE_IMAGE_EXACT);
+                                    ?>
+                                    <img src="<?=$resizedImage["src"]?>" alt="">
+                                <?php else:?>
+                                    <img src="<?=SITE_TEMPLATE_PATH?>/img/no_photo_left_block.jpg" alt="">
+                                <?php endif;?>
+                            </div>
+                            <div class="name-block"><a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a></div>
+                            <div class="pos-block"><?=$arResult['DISPLAY_PROPERTIES']['POSITION']['VALUE']?>,
+                                <?=$arResult['DISPLAY_PROPERTIES']['COMPANY']['VALUE']?></div>
+                        </div>
+                        <div class="text-block"><?=TruncateText($arItem["PREVIEW_TEXT"], 150);?></div>
                     </div>
-                    <div class="name-block"><a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a></div>
-                    <div class="pos-block"><?=$arItem["DISPLAY_PROPERTIES"]['POSITION']['VALUE']?>,<?=$arItem["DISPLAY_PROPERTIES"]['COMPANY']['VALUE']?></div>
-                </div>
-                <div class="text-block">
-                    <?
-                    $str = $arItem["PREVIEW_TEXT"];
-                    echo TruncateText($str, 150);
-                    ?>
                 </div>
             </div>
-        </div>
+        <?endforeach;?>
     </div>
-    <?endforeach;?>
 </div>
 
 
